4a93612be7204c11ed1fa1e8546ae5a6a5fe35e5
Rohit Kulkarni <rdk4online@gmail.com>
2018-12-19 15:37:23 +0530

    Project orson with mysql created

---
+++ Projectorson/src/main/java/com/orson/springframework/ProjectorsonApplication.java
@@ -0,0 +1,14 @@
+package com.orson.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectorsonApplication.class, args);
+	}
+
+}
+
